e5674f7165c5a14edfc5d529ab82d91d2b136ea6
oZ0614 <omari.wallace@gmail.com>
2014-04-04 15:36:58 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var swig = require('swig');
+
+var app = express();
+
+// Swig
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.set('view cache', false);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/purchase', routes.purchase)
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ models/index.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ecommerce');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+var Order;
+
+var Schema = mongoose.Schema;
+
+var orderSchema = new Schema({
+  name: String,
+  address: String,
+  method: String,
+  creditCard: Number,
+})
